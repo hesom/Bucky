3665d455bcba4105e469b67f32061c06baa25ced
= <=>
2018-02-01 22:43:30 +0100

    Put commit hash in code block because it may look better

--- bot.js
+++ bot.js
@@ -67,7 +67,7 @@ app.post('/', (req, res)=>{
         var commitHash = commit.hash;
         var commitLink = commit.links.html.href;
         var shortHash = commitHash.substring(0, 7);
-        var fieldName = `[${shortHash}](${commitLink})`;
+        var fieldName = `[\`${shortHash}\`](${commitLink})`;
         description += `\n${fieldName} ${commit.message.split('\n')[0].trunc(100)}`;
       }
       embed.setDescription(description);
